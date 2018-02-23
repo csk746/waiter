dedbd0cca3569f1a3d6a12d07dee2435f3beb677
Lama <lama@Lamaui-MacBook-Pro.local>
2018-02-23 14:40:28 +0900

    문자발송코드, 고객 서버 api

---
+++ waiter_server/src/main/java/com/daou/waiter/sms/SMS.java
@@ -0,0 +1,150 @@
+package com.daou.waiter.sms;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Getter
+@Setter
+
+public class SMS {
+    /**
+     * primary key
+     */
+    private String cmid;
+
+
+    private Long receiverId;
+
+    /**
+     * 다우기술 서버에서 정의한 message Id
+     */
+    private String umid;
+
+    /**
+     * 데이터 타입 (0 SMS / 1 WAP / 2 FAX / 3 PHONE / 4 SMS_INBOUND / 5 MMS)
+     */
+    private int msgType;
+
+    /**
+     * 데이터 발송상태 (0 대기 / 1 발송중 / 2 발송완료 / 3 에러)
+     */
+    private int status;
+
+    /**
+     * 입력 시간
+     */
+    private Date requestTime;
+
+    /**
+     * 발송(예약) 시간
+     */
+    private Date sendTime;
+
+    /**
+     * 발송 완료 시간
+     */
+    private Date reportTime;
+
+    /**
+     * 받는 사람 전화 번호
+     */
+    private String destPhone;
+
+    /**
+     *  받는 사람 이름
+     */
+    private String destName;
+
+    /**
+     * 보내는 사람 전화 번호
+     */
+    private String sendPhone;
+
+    /**
+     * 보내는 사람 이름
+     */
+    private String sendName;
+
+    /**
+     * (FAX/MMS) 제목 (최대 60byte)
+     * (SMS_INBOUND) 데이터 내용
+     */
+    private String subject;
+
+    /**
+     * 데이터 내용
+     */
+    private String msgBody;
+
+    /**
+     * (WAP) URL 주소
+     */
+    private String wapUrl;
+
+    /**
+     * (FAX) 커버 발송 옵션
+     */
+    private int coverFlag;
+
+    /**
+     * (PHONE) 실패시 문자 전송 옵션
+     */
+    private int smsFlag;
+
+    /**
+     * (PHONE) 응답 받기 선택
+     */
+    private int replyFlag;
+
+    /**
+     * (FAX/PHONE) 재시도 회수 (5~10분 간격 : 최대 3회)
+     */
+    private int retryCnt;
+
+    /**
+     * (PHONE/FAX/MMS) 파일 전송시 파일 이름
+     */
+    private String faxFile;
+
+    /**
+     * (PHONE) 음성 시나리오 파일 이름
+     */
+    private String vxmlFile;
+
+    /**
+     * 데이터의 결과 리포트
+     */
+    private int callStatus;
+
+    /**
+     * (FAX) 발송 페이지 수
+     */
+    private int usePage;
+
+    /**
+     * (PHONE) 발송 소요 시간 (단위 : 초)
+     */
+    private int useTime;
+
+    /**
+     * (PHONE) 설문조사 응답 값
+     */
+    private int snResult;
+
+    /**
+     * 통신사 정보(SKT, KTF, LGT)
+     */
+    private String wapInfo;
+
+    /**
+     * Client Indexed Info
+     * ( 특수기호 \/:*?"<>|. 를 사용할 사용할 수 없음 )
+     */
+    private String cinfo;
+
+    /**
+     * 처리결과 여부 판단
+     */
+    private boolean sendFlag;
+}
---
+++ waiter_server/src/main/java/com/daou/waiter/sms/SmsServiceImpl.java
@@ -0,0 +1,137 @@
+package com.daou.waiter.sms;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.json.JSONObject;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+@Slf4j
+@Service
+public class SmsServiceImpl implements SmsService {
+
+    private String loginUrl = "https://portal.daou.co.kr";
+    private String smsUrl = "https://portal.daou.co.kr/api/sms";
+
+    @Autowired
+    ObjectMapper objectMapper ;
+    String portalId = "daouoffice@daou.co.kr";
+    String password = "daouware8707!";
+
+    private HashMap<String, String> sessionMap = new HashMap<>();
+
+    @Override
+    public void smsSend(SmsModel smsModel) {
+        smsModel.setSubject("aaaa");
+        smsModel.setContent("bbbb");
+        smsModel.getMessageType();
+        String body = null ;
+        System.out.println(this.loginUrl + "/api/user/session");
+        String cookie = doUserLogin(this.loginUrl, this.portalId, this.password,  this.loginUrl + "/api/user/session");
+
+        try {
+            body = this.objectMapper.writeValueAsString(smsModel);
+        } catch (JsonProcessingException e) {
+            log.info("sms body object to string error" );
+        }
+        System.out.println("cookie "+cookie);
+        String res = commonRestApiCall(this.smsUrl, body, HttpMethod.POST, cookie);
+        log.info("res :" + res);
+    }
+
+
+    private String getSessionKey(String url, String id, String pw){
+        return url + "-"+ id + ":" + pw ;
+    }
+
+    @Override
+    public String doUserLogin(String siteUrl, String loginId, String password, String sessionCheckUrl) {
+
+        String sessionKey = this.getSessionKey(siteUrl, loginId, password);
+        String session = this.sessionMap.get(sessionKey);
+        if ( session != null){
+            boolean valid = this.sessionValidation(session, sessionCheckUrl);
+            log.info("session validation check :: " + sessionKey + " --- check Url " + sessionCheckUrl + " status :: " + valid );
+            if ( valid ){
+                log.info("user session is alive return old session");
+                return session ;
+            }
+        }
+
+        RestTemplate restTemplate = new RestTemplate();
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("username", loginId);
+        jsonObj.put("password", password);
+
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.set("Accept-Language", "ko");
+
+        HttpEntity<String> httpEntity = new HttpEntity<String>(jsonObj.toString(), headers);
+
+        String adminSsoCookie = null;
+        System.out.println(siteUrl + "/go/ad/api/login");
+        ResponseEntity<String> response = restTemplate.postForEntity(siteUrl + "/api/login", httpEntity, String.class);
+        HttpHeaders responseHeader = response.getHeaders();
+        for (String cookie : responseHeader.get("Set-Cookie")) {
+            if (cookie.contains("GOSSOcookie")) {
+                adminSsoCookie = cookie;
+            }
+        }
+
+        this.sessionMap.put(sessionKey, adminSsoCookie);
+        return adminSsoCookie;
+    }
+    private boolean sessionValidation(String session, String url) {
+
+        HttpHeaders headers = new HttpHeaders();;
+        headers.setContentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8));
+
+        headers.set("Cookie", session);
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> httpEntity = null ;
+        httpEntity = new HttpEntity<String>(headers);
+
+        try{
+            ResponseEntity<String> res = restTemplate.exchange(url,  HttpMethod.GET, httpEntity, String.class);
+            int status = res.getStatusCodeValue();
+            return (status / 100) == 2 ;
+        }catch(Exception e){
+            log.info("rest client exception : " + e.getMessage());
+            return false ;
+        }
+    }
+    public String commonRestApiCall(String url, String body, HttpMethod method, String cookie){
+
+        HttpHeaders headers = new HttpHeaders();;
+        headers.setContentType(new MediaType(MediaType.APPLICATION_JSON, StandardCharsets.UTF_8));
+
+        if ( cookie != null){
+            headers.set("Cookie", cookie);
+        }
+
+        log.info("request Url : " + url + ", method : " + method.toString());
+        log.info("request body " );
+        log.info( body );
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpEntity<String> httpEntity = null ;
+        if ( body == null){
+            httpEntity = new HttpEntity<String>(headers);
+        }
+        else{
+            httpEntity = new HttpEntity<String>(body, headers);
+        }
+
+        ResponseEntity<String> res = restTemplate.exchange(url,  method, httpEntity, String.class);
+        return res.getBody();
+    }
+}
